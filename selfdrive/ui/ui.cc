bf0d1858ce5b3994edd13b960008953c0f2e9153
bensonkwek <34688817+bensonkwek@users.noreply.github.com>
2021-12-30 17:04:34 -0800

    reduce brightness to 50%

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -306,7 +306,7 @@ void Device::updateBrightness(const UIState &s) {
   float clipped_brightness = BACKLIGHT_OFFROAD;
   if (s.scene.started) {
     // Scale to 0% to 100%
-    clipped_brightness = 100.0 * s.scene.light_sensor;
+    clipped_brightness = 50.0 * s.scene.light_sensor;
 
     // CIE 1931 - https://www.photonstophotos.net/GeneralTopics/Exposure/Psychometric_Lightness_and_Gamma.htm
     if (clipped_brightness <= 8) {
