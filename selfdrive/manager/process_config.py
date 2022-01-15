d00bdb18001d785b6a478a7016ac13a6887ff928
bensonkwek <34688817+bensonkwek@users.noreply.github.com>
2022-01-15 09:43:30 -0800

    disable auto upload

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -35,7 +35,7 @@ procs = [
   PythonProcess("timezoned", "selfdrive.timezoned", enabled=TICI, persistent=True),
   PythonProcess("tombstoned", "selfdrive.tombstoned", enabled=not PC, persistent=True),
   PythonProcess("updated", "selfdrive.updated", enabled=not PC, persistent=True),
-  PythonProcess("uploader", "selfdrive.loggerd.uploader", persistent=True),
+  #PythonProcess("uploader", "selfdrive.loggerd.uploader", persistent=True),
   PythonProcess("mapd", "selfdrive.mapd.mapd"),
 
   # EON only
